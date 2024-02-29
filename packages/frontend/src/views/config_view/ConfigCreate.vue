e3db2bc88b2604db54332b36d6ac492b77d31d91
CSPSY <1554830585@qq.com>
2024-02-29 17:41:43 +0800

    feat: 创建站点 dialog 框框建立

--- packages/frontend/src/views/config_view/ConfigCreate.vue
+++ packages/frontend/src/views/config_view/ConfigCreate.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { reactive, ref, onMounted } from 'vue';
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
 const formData = ref({
     configName: '',
@@ -20,6 +25,24 @@ const editorRef = ref();
 let editor;
 
 onMounted(() => {
+    self.MonacoEnvironment = {
+        getWorker(_, label) {
+            if (label === 'json') {
+            return new jsonWorker()
+            }
+            if (label === 'css' || label === 'scss' || label === 'less') {
+            return new cssWorker()
+            }
+            if (label === 'html' || label === 'handlebars' || label === 'razor') {
+            return new htmlWorker()
+            }
+            if (label === 'typescript' || label === 'javascript') {
+            return new tsWorker()
+            }
+            return new editorWorker()
+        }
+    }
+
     editor = monaco.editor.create(editorRef.value, {
         value: '',
         language: 'json',
--- packages/frontend/src/views/site_view/Site.vue
+++ packages/frontend/src/views/site_view/Site.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { reactive, ref, nextTick } from 'vue';
 
 const inputSearch = ref('');
 
@@ -9,6 +9,72 @@ const pageInfo = reactive({
     total: 20
 })
 
+// 新建站点对话框
+const dialogSiteNewVisible = ref(false);
+
+const formData = ref({
+    siteName: '',
+    configRelate: [],
+});
+
+const formRules = {
+    siteName: [{ required: true, message: '请输入站点名', trigger: 'change' }],
+    configRelate: [{ type: 'array', required: true, message: '请先完成此项', trigger: 'blur' }],
+};
+
+// 关联配置 --- 搜索
+const filterMethod = (query, item) => {
+    return item.label.includes(query);
+}
+
+const data = reactive([
+    {
+        key: 1,
+        label: "简易版个人简介",
+    },
+    {
+        key: 2,
+        label: "简易版",
+    },
+    {
+        key: 3,
+        label: "简介",
+    },
+    {
+        key: 4,
+        label: "个人简介",
+    },
+    {
+        key: 5,
+        label: "profile",
+    },
+]);
+
+
+// 动态增减域名
+const inputValue = ref('');
+const dynamicTags = ref([]);
+const inputVisible = ref(false);
+const InputRef = ref();
+
+const handleClose = (tag) => {
+    dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1);
+};
+
+const showInput = () => {
+    inputVisible.value = true;
+    nextTick(() => {
+        InputRef.value.focus();
+    });
+};
+
+const handleInputConfirm = () => {
+    if (inputValue.value) {
+        dynamicTags.value.push(inputValue.value);
+    }
+    inputVisible.value = false;
+    inputValue.value = '';
+};
 </script>
 
 <template>
@@ -34,19 +100,71 @@ const pageInfo = reactive({
                             <span class="titleSmall">
                                 我的站点
                             </span>
-                            <el-button type="primary" plain>新建</el-button>
+                            <!-- 新建站点 -->
+                            <el-button type="primary" plain @click="dialogSiteNewVisible = true">新建</el-button>
+                            <el-dialog v-model="dialogSiteNewVisible" title="创建站点" width="600" draggable>
+                                <section class="input-area">
+                                    <el-form :model="formData" :rules="formRules" ref="form" label-position="top">
+                                        <el-form-item label="站点名" prop="siteName">
+                                            <el-input v-model="formData.siteName" placeholder="请输入"></el-input>
+                                        </el-form-item>
+                                        <el-form-item label="关联配置" prop="configRelate">
+                                            <el-transfer
+                                                v-model="formData.configRelate"
+                                                filterable
+                                                :filter-method="filterMethod"
+                                                filter-placeholder="请输入关键词搜索"
+                                                :titles="['未关联', '已关联']"
+                                                :data="data"
+                                            />
+                                        </el-form-item>
+                                        <el-form-item label="域名">
+                                            <el-tag
+                                                v-for="tag in dynamicTags"
+                                                :key="tag"
+                                                closable
+                                                :disable-transitions="false"
+                                                @close="handleClose(tag)"
+                                            >
+                                            {{ tag }}
+                                            </el-tag>
+                                            <el-input
+                                                v-if="inputVisible"
+                                                ref="InputRef"
+                                                v-model="inputValue"
+                                                @keyup.enter="handleInputConfirm"
+                                                @blur="handleInputConfirm"
+                                            />
+                                            <el-button v-else class="button-new-tag" size="small" @click="showInput">
+                                                + 域名
+                                            </el-button>
+                                        </el-form-item>
+                                    </el-form>
+                                </section>
+                                
+                                <template #footer>
+                                <div class="dialog-footer">
+                                    <el-button @click="dialogSiteNewVisible = false">
+                                        取消
+                                    </el-button>
+                                    <el-button type="primary" @click="() => {console.log(formData)}">
+                                        确认
+                                    </el-button>
+                                </div>
+                                </template>
+                            </el-dialog>
                         </div>
-                        <el-input v-model="inputSearch" placeholder="输入关键词，回车查找配置" clearable />
+                        <el-input v-model="inputSearch" placeholder="输入关键词，回车查找站点" clearable />
                     </el-card>
                 </div>
                 <div class="content-bottom">
                     <div style="padding: 16px 0;">
-                    <el-row>
+                    <el-row :gutter="14">
                         <el-col
                             v-for="(o, index) in 5"
                             :key="o"
                             :span="8"
-                            style="padding: 0 12px; margin-bottom: 32px;"
+                            style="margin-bottom: 32px;"
                         >
                             <el-card
                                 :body-style="{
@@ -156,6 +274,12 @@ const pageInfo = reactive({
     align-items: center;
 }
 
+.input-area {
+    margin-top: 12px;
+    display: flex;
+    flex-direction: column;
+}
+
 /* footer 样式 */
 .footer {
     padding: 16px;
@@ -170,7 +294,7 @@ const pageInfo = reactive({
     color: #0052d9;
 }
 
-.el-card ::v-deep .el-card__header {
+.el-card :deep(.el-card__header) {
     padding-top: 0px;
     padding-bottom: 0px;
     height: 56px;
